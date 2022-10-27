2c23ca10ff1874c42d954a2f14ee2905ad530f31
Vinicius <vinicius_henrique96@hotmail.com>
2022-10-27 12:51:12 -0300

    :sparkles: feat: Implementando uma API de geolocalização

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,19 @@
-var map = L.map("map").setView([51.505, -0.09], 13);
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    console.log(position);
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    const coords = [latitude, longitude];
+    const map = L.map("map").setView(coords, 13);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map);
+    L.marker(coords).addTo(map);
+  },
+  function () {
+    console.log("Could not get your position");
+  }
+);
